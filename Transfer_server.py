bba27aae2af7c780c58f42ccf8a88a66b723cc0a
YuhL6 <49777375+YuhL6@users.noreply.github.com>
2020-01-05 00:36:49 +0800

    Update Transfer_server.py

--- Transfer_server.py
+++ Transfer_server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 
 def recv_size(sock, count):
@@ -11,80 +12,60 @@ def recv_size(sock, count):
     return buf
 
 
-class Message:
-    def __init__(self, data: str):
-        data = data.split('\r\n')
-        self.user_id = data[0].split(':')[-1]
-        self.function = int(data[1].split(':')[-1])
-
-
 class transfer_server:
     def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.recvS = None
-        self.sendS = None
+        self.transfer_socket = None
+        self.command_socket = None
+        self.accept_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.accept_socket.bind(('', 12346))
+        self.next = False
 
-    def accept(self):
-        print('start server')
-        self.socket.bind(('', 12345))
-        self.socket.listen(10)
-        First = False
+    def connectToCS(self):
+        tmp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tmp.bind(('', 12345))
+        tmp.listen(10)
+        first = False
         while True:
-            conn, addr = self.socket.accept()
+            conn, addr = tmp.accept()
             data = conn.recv(1024)
-            print(data)
             data = data.decode()
-            if data == 'it\'s me':
-                self.recvS = conn
-                First = True
-            if data == 'hello' and First:
-                self.sendS = conn
-                self.sendS.setblocking(False)
+            if data == 'transfer':
+                self.transfer_socket = conn
+                self.transfer_socket.setblocking(False)
+                first = True
+            if data == 'command' and first:
+                self.command_socket = conn
                 break
-        self.recv()
+        print('connection to CS established')
+        task = threading.Thread(target=self.recv_cmd)
+        task.start()
+        self.recv_cmd()
 
-    def recv(self):
+    def recv_cmd(self):
         while True:
-            conn, addr = self.socket.accept()
-            data = ''
+            cmd = self.command_socket.recv(1024)
+            if cmd == b'next':
+                self.next = True
+
+    def recv_data(self):
+        while True:
+            conn, addr = self.accept_socket.accept()
             while True:
                 tmp = conn.recv(2048)
-                tmp = tmp.decode()
-                data += tmp
-                if tmp[len(tmp) - 3: len(tmp) - 1] == '\r\n':
+                self.transfer_socket.send(tmp)
+                try:
+                    tmp = self.transfer_socket.recv(2048)
+                    conn.send(tmp)
+                except:
+                    pass
+                if self.next:
+                    self.next = False
                     break
-            self.sendS.send(data)
-            message = Message(data)
-            if message.function == 200:
-                while True:
-                    data = conn.recv(16)
-                    self.sendS.send(data)
-                    data = int(data.decode())
-                    data = recv_size(conn, data).encode()
-                    self.sendS.send(data)
-            if message.function == 201:
-                while True:
-                    data = conn.recv(16)
-                    self.sendS.send(data)
-                    data = int(data.decode())
-                    data = recv_size(conn, data).encode()
-                    self.sendS.send(data)
-                    try:
-                        data = self.recvS.recv(1024)
-                        conn.send(data)
-                    except:
-                        continue
-
-            elif data == b'403':
-                conn.send(b'No face recognized')
-            elif data == b'404':
-                conn.send(b'Not the same person')
-            elif data == b'405':
-                conn.send(b'User id not exists')
             conn.close()
 
     def start_server(self):
-        self.accept()
+        print('start server')
+        self.connectToCS()
 
 
 if __name__ == '__main__':
